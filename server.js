7d1be8a89870968359724921addaa44c215f4ec2
Uthman AHMAD Muhammad <uthmana.muhammad@gmail.com>
2021-08-19 22:28:03 +0300

    ! qa!

--- server.js
+++ server.js
@@ -5,20 +5,19 @@ const cors = require("cors");
 const app = express();
 const path = __dirname + "/views/";
 
-const db = require("./models");
-
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch((err) => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+//const db = require("./models");
+// db.mongoose
+//   .connect(db.url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to the database!");
+//   })
+//   .catch((err) => {
+//     console.log("Cannot connect to the database!", err);
+//     process.exit();
+//   });
 
 app.use(express.static(path));
 var corsOptions = {
